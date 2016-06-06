0a9befe688bb4394ac4b8a95f824f996e9b60ce9
Dianyo <me1996017@gmail.com>
2016-06-06 19:32:49 +0800

    remove strange google api

--- app/src/main/java/com/example/dianyo/itraveler/StartActivity.java
+++ app/src/main/java/com/example/dianyo/itraveler/StartActivity.java
@@ -33,10 +33,6 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
-
 public class StartActivity extends Activity {
     private ListView listview;
     private TextView total_budget;
